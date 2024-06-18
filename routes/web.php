0ec180d0c679080a4e79271f89dcba224c086f5e
GolubevaSofja <133961497+GolubevaSofja@users.noreply.github.com>
2024-06-18 18:24:15 +0300

    removing Statamic CMS and adding Laravel Breeze
    additionally added some pages to check authentication and authorization system

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('frontend');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-/*Route::get('/{vue_capture?}', function () {
-    return view('welcome');
-})->where('vue_capture', '[\/\w\.-]*');*/
+Route::get('/home', function () {
+    return Inertia::render('HomePage');
+})->middleware(['auth', 'verified'])->name('home');
 
-Route::view('/cp', 'vendor.statamic.cp.index')->name('statamic.cp');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-// Route::statamic('example', 'example-view', [
-//    'title' => 'Example'
-// ]);
+require __DIR__.'/auth.php';
